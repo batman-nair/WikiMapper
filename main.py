753669a74e4e51e296cf13f80acb53721f2fe180
Arjun Nair <arjunnair.mec@gmail.com>
2021-04-13 20:53:47 +0530

    Finalize some stuff

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def process_interlink_scores(target_link: str, all_links: set, max_depth: int = 
     for depth in range(1, max_depth+1):
         print('Parsing at depth', depth)
         with open('source_links', 'r', encoding='utf-8') as source_file, open('generated_links', 'w', encoding='utf-8') as generated_file:
-            for extracted_links in pool.imap(wiki_util.extract_wiki_links, source_file, chunksize=100):
+            for extracted_links in pool.imap(wiki_util.extract_wiki_links, source_file):
                 generated_file.write('\n'.join(extracted_links))
 
         with open('generated_links', 'r', encoding='utf-8') as generated_file, open('source_links', 'w', encoding='utf-8') as source_file:
@@ -34,18 +34,30 @@ def generate_link_to_name_map(all_links):
 
 if __name__ == '__main__':
     all_links = []
+    output_directory = 'csv'
     with open('country_links.txt', 'r') as countries_file:
         all_links = set(countries_file.read().splitlines())
 
     link_to_name_map = generate_link_to_name_map(all_links)
+    link_total_scores = dict()
+
+    os.makedirs(output_directory, exist_ok=True)
 
     for link in all_links:
         current_scores = process_interlink_scores(link, all_links, max_depth=2)
-
-        print('Calculated scores for', link_to_name_map[link], 'writing to csv file.')
-        csv_file = os.path.join(['csv', link_to_name_map[link]+'.csv'])
+        print('Calculated scores for', link_to_name_map[link], ', writing to csv file.')
+        csv_file = os.path.join(output_directory, link_to_name_map[link]+'.csv')
+        total_score = 0
         with open(csv_file, 'w', encoding='utf-8') as score_file:
-            link_name = link_to_name_map(link)
+            score_file.write('Source,Target,Weight\n')
+            link_name = link_to_name_map[link]
             for link, score in current_scores.items():
+                total_score += score
                 connection_name = link_to_name_map[link]
                 score_file.write(','.join([link_name, connection_name, str(score)]) + '\n')
+        link_total_scores[link_to_name_map[link]] = total_score
+
+    with open(os.path.join(output_directory, 'node_list')) as node_file:
+        node_file.write('Id,Label,Count')
+        for entry_name, total_score in entry_total_scores.items():
+            node_file.write(','.join(entry_name, entry_name, int(total_score)))
