d439cafef5ab59f6c3c33f1766160d291728c400
Arjun Nair <arjunnair.mec@gmail.com>
2021-04-13 18:14:50 +0530

    Create thhreads 2*cpu_count; add csv directory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from multiprocessing import Pool
+import os
+from multiprocessing import Pool, cpu_count
 
 import src.wiki_util as wiki_util
 
@@ -7,7 +8,7 @@ def process_interlink_scores(target_link: str, all_links: set, max_depth: int = 
     with open('source_links', 'w', encoding='utf-8') as source_file:
         source_file.write(target_link)
 
-    pool = Pool(10)
+    pool = Pool(cpu_count()*2)
     for depth in range(1, max_depth+1):
         print('Parsing at depth', depth)
         with open('source_links', 'r', encoding='utf-8') as source_file, open('generated_links', 'w', encoding='utf-8') as generated_file:
@@ -25,7 +26,7 @@ def process_interlink_scores(target_link: str, all_links: set, max_depth: int = 
     return interlink_scores
 
 def generate_link_to_name_map(all_links):
-    pool = Pool(10)
+    pool = Pool(cpu_count()*2)
     # get_link_name = lambda link: (link, wiki_util.get_page_title(link))
     link_to_name_list = pool.map(wiki_util.get_page_title, all_links)
     link_to_name_map = dict(link_to_name_list)
@@ -42,7 +43,8 @@ if __name__ == '__main__':
         current_scores = process_interlink_scores(link, all_links, max_depth=2)
 
         print('Calculated scores for', link_to_name_map[link], 'writing to csv file.')
-        with open(link_to_name_map[link]+'.csv', 'w', encoding='utf-8') as score_file:
+        csv_file = os.path.join(['csv', link_to_name_map[link]+'.csv'])
+        with open(csv_file, 'w', encoding='utf-8') as score_file:
             link_name = link_to_name_map(link)
             for link, score in current_scores.items():
                 connection_name = link_to_name_map[link]
